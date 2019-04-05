ab1bef7bb045b9bc48d8288bb38e40161dbbe5d6
amarjagrup <a30jagr@siena.edu>
2019-04-04 21:42:58 -0400

    card class

---
+++ Card.java
@@ -0,0 +1,11 @@
+import java.awt.Image;
+/**
+ * Write a description of class ttr here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class  Card
+{
+   public abstract Image getImage();
+}
