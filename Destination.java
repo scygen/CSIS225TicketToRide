bf63980161bf5c8b5c119fec8e7128757d8a9cbe
amarjagrup <a30jagr@siena.edu>
2019-04-04 21:43:38 -0400

    Destination class

---
+++ Destination.java
@@ -0,0 +1,29 @@
+
+/**
+ * Enumeration class Destination - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Destination
+{
+    BROOKLYN,
+    CHELSEA,
+    EMPIRE,
+    TIMES,
+    CENTRAL,
+    CHINA,
+    GRAMERCY,
+    EAST,
+    LINCOLN,
+    WALLSTREET,
+    UN,
+    MIDTOWN,
+    SOHO,
+    LOWEREASTSIDE,
+    GREENWICH,
+    
+    
+    
+    
+}
