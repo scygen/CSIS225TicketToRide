6ce7624b501cc9ba95639a803174699bee4722fe
amarjagrup <a30jagr@siena.edu>
2019-04-20 09:46:34 -0400

    route

--- RouteColor.java
+++ RouteColor.java
@@ -5,5 +5,7 @@
  * @version 1.0
  */
 public enum RouteColor{
-  ORANGE, RED, PINK, BLACK, BLUE, GREY, GREEN
+   //added rainbow because all cards color match
+   //rpute color except for rainbow
+  ORANGE, RED, PINK, BLACK, BLUE, GREY, GREEN,RAINBOW
 }
